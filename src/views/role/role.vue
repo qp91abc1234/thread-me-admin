a38a6b1c0bd5590e282e8fbbc669707acac660d1
zcc <350018736@qq.com>
2026-01-01 16:03:33 +0800

    代码缓存

--- src/views/role/role.vue
+++ src/views/role/role.vue
@@ -184,28 +184,21 @@ init()
         <el-table-column prop="createTime" label="创建时间" min-width="100" show-overflow-tooltip />
         <el-table-column label="操作" min-width="160" fixed="right" align="center">
           <template #default="{ row }">
-            <template v-if="!row.isSystem">
-              <el-button type="primary" link :icon="Edit" @click="handleEdit(row)">编辑</el-button>
-              <el-button
-                type="success"
-                link
-                :icon="Key"
-                @click="handlePermissionConfig(row, 'menu')"
-              >
-                菜单权限
-              </el-button>
-              <el-button
-                type="warning"
-                link
-                :icon="Connection"
-                @click="handlePermissionConfig(row, 'api')"
-              >
-                API权限
-              </el-button>
-              <el-button type="danger" link :icon="Delete" @click="handleDelete(row)">
-                删除
-              </el-button>
-            </template>
+            <el-button type="primary" link :icon="Edit" @click="handleEdit(row)">编辑</el-button>
+            <el-button type="success" link :icon="Key" @click="handlePermissionConfig(row, 'menu')">
+              菜单权限
+            </el-button>
+            <el-button
+              type="warning"
+              link
+              :icon="Connection"
+              @click="handlePermissionConfig(row, 'api')"
+            >
+              API权限
+            </el-button>
+            <el-button type="danger" link :icon="Delete" @click="handleDelete(row)">
+              删除
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -178,7 +178,6 @@ init()
         <el-table-column prop="status" label="状态" width="100" align="center">
           <template #default="{ row }">
             <el-switch
-              :disabled="row.isSystem"
               v-model="row.status"
               :active-value="1"
               :inactive-value="0"
@@ -189,12 +188,10 @@ init()
         <el-table-column prop="createTime" label="创建时间" min-width="170" show-overflow-tooltip />
         <el-table-column label="操作" width="150" fixed="right" align="center">
           <template #default="{ row }">
-            <template v-if="!row.isSystem">
-              <el-button type="primary" link :icon="Edit" @click="handleEdit(row)">编辑</el-button>
-              <el-button type="danger" link :icon="Delete" @click="handleDelete(row)"
-                >删除
-              </el-button>
-            </template>
+            <el-button type="primary" link :icon="Edit" @click="handleEdit(row)">编辑</el-button>
+            <el-button type="danger" link :icon="Delete" @click="handleDelete(row)"
+              >删除
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
